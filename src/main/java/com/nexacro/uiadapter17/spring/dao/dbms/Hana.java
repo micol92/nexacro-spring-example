1d0fbf7eb5b905fa835f3165db70cb08843b21c9
jae-hong-cheon <jae.hong.cheon@sap.com>
2023-05-07 12:04:18 +0900

    point to Hana database

---
+++ src/main/java/com/nexacro/uiadapter17/spring/dao/dbms/Hana.java
@@ -0,0 +1,13 @@
+package com.nexacro.uiadapter17.spring.dao.dbms;
+
+import com.nexacro.uiadapter17.spring.dao.AbstractDbms;
+import com.nexacro.uiadapter17.spring.dao.DbColumn;
+
+public class Hana extends AbstractDbms {
+    public Hana() {
+    }
+
+    public void handleColumnDataType(DbColumn column) {
+        // TODO
+    }
+}
